561f3594e7650d209574cca3722aa7daa9ea2a00
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2015-12-28 00:41:54 +0000

    minify slowly but usefully

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ coffee -b -c make-qr.coffee   &&              \
 
 /usr/bin/time -f "bundled in min %E seconds"  \
   browserify --fast paperbank.js -o bundle.js
+
+
+/usr/bin/time -f "minified in min %E seconds" uglifyjs bundle.js > bundle.min.js
